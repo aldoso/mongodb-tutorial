8d0622dd9c2fdbda917ca936130550a2d48ba33a
aldoso <alexdobras@gmail.com>
2017-09-28 20:40:19 +0300

    add mocha test

---
+++ test/demo_test.js
@@ -0,0 +1,13 @@
+//MOCHA testing
+
+const assert = require('assert');
+
+// Describe our tests
+describe('testing mocha', function(){
+
+  // Create tests
+  it('adds numbers that equal to 5', function(){
+    assert(2 + 3 === 5);
+  });
+
+});
